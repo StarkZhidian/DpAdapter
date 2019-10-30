6fae826e713595a936dc531487cc12f84973a58c
hongweiqiu <hongweiqiu@tencent.com>
2019-10-30 15:33:59 +0800

    Improve codes

--- demo_app/src/main/java/com/zhidian/app/MainActivity.java
+++ demo_app/src/main/java/com/zhidian/app/MainActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.vane.dpadapter.DisplayMetricsUtils;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DisplayMetricsUtils.adjustDisplayMetricsDensity(this);
         setContentView(R.layout.activity_main);
     }
 }
